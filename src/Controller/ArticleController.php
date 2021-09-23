5dee9d61df3201edd0a81b0e22e6bf22bd8bf645
Chris-1042330 <1042330@mborijnland.nl>
2021-09-23 10:06:19 +0200

    stuff

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -17,24 +17,24 @@ class ArticleController extends AbstractController
 
         return $this->render('articles/index.html.twig', array('articles' => $articles));
     }
-//    public function createArticle(): Response{
-//        // you can fetch the EntityManager via $this->getDoctrine()
-//        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
-//        $entityManager = $this->getDoctrine()->getManager();
-//
-//        $article = new Article();
-//        $article->setTitle('Keyboard');
-//        $article->setBody('aaaaaaaa');
-//
-//        // tell Doctrine you want to (eventually) save the Product (no queries yet)
-//        $entityManager->persist($article);
-//
-//        // actually executes the queries (i.e. the INSERT query)
-//        $entityManager->flush();
-//
-//        return new Response('Saved new product with id '.$article->getId());
-//    }
-//
+    public function createArticle(): Response{
+        // you can fetch the EntityManager via $this->getDoctrine()
+        // or you can add an argument to the action: createProduct(EntityManagerInterface $entityManager)
+        $entityManager = $this->getDoctrine()->getManager();
+
+        $article = new Article();
+        $article->setTitle('Keyboard');
+        $article->setBody('aaaaaaaa');
+
+        // tell Doctrine you want to (eventually) save the Product (no queries yet)
+        $entityManager->persist($article);
+
+        // actually executes the queries (i.e. the INSERT query)
+        $entityManager->flush();
+
+        return new Response('Saved new product with id '.$article->getId());
+    }
+
 //    /**
 //     * @Route("/article/{id}", name="article_show")
 //     */
--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 /**
@@ -19,6 +21,7 @@ class Category
 
     /**
      * @ORM\Column(type="integer")
+     * @ORM\OneToMany(targetEntity="App\Entity\Product", mappedBy="category")
      */
     private $content_id;
 
@@ -26,6 +29,16 @@ class Category
      * @ORM\Column(type="datetime")
      */
     private $create_date;
+
+    /**
+     * @ORM\OneToMany(targetEntity=Content::class, mappedBy="ida")
+     */
+    private $content_ida;
+
+    public function __construct()
+    {
+        $this->content_ida = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -52,6 +65,36 @@ class Category
     public function setCreateDate(\DateTimeInterface $create_date): self
     {
         $this->create_date = $create_date;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Content[]
+     */
+    public function getContentIda(): Collection
+    {
+        return $this->content_ida;
+    }
+
+    public function addContentIda(Content $contentIda): self
+    {
+        if (!$this->content_ida->contains($contentIda)) {
+            $this->content_ida[] = $contentIda;
+            $contentIda->setIda($this);
+        }
+
+        return $this;
+    }
+
+    public function removeContentIda(Content $contentIda): self
+    {
+        if ($this->content_ida->removeElement($contentIda)) {
+            // set the owning side to null (unless already changed)
+            if ($contentIda->getIda() === $this) {
+                $contentIda->setIda(null);
+            }
+        }
 
         return $this;
     }
